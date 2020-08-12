1715e3285a65bf92a38b8bd292e38db52749122d
Aditya Sidharta <aditya.sdrt@gmail.com>
2020-08-13 01:04:51 +0800

    [Adi] Add hw4

--- programming_language_b/week_4/hw4.rkt
+++ programming_language_b/week_4/hw4.rkt
@@ -5,3 +5,20 @@
 
 ;; put your code below
 
+(define (sequence low high stride) (if (> low high) '() (append (list low) (sequence (+ low stride) high stride))))
+
+(define (string-append-map xs suffix) (map (lambda (x) (string-append x suffix)) xs))
+
+(define (list-nth-mod xs n)
+  (let ([i (remainder n (length xs))])
+  (if (> 0 n) (error "list-nth-mod: negative number")
+      (if (= 0 (length xs)) (error "list-nth-mod: empty list")
+          (car (list-tail xs i))))))
+
+(define (stream-for-n-steps s n)
+  (if (>= 0 n) '()
+      (append (list (car (s))) (stream-for-n-steps (cdr (s)) (- n 1)))))
+
+(define funny-number-stream
+         (letrec ([acc (lambda (x) (cons (if (= (remainder x 5) 0) (- x) x) (lambda () (acc (+ x 1)))))])
+           (lambda () (acc 1))))
